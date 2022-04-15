2c69d21b7889b53c99282d3868b39e04ee63f0e3
Daniele <tigre9@gmail.com>
2022-04-15 12:12:28 +0200

    create module for only section

---
+++ src/app/features/anagrafica/anagrafica.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnagraficaRoutingModule } from './anagrafica-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnagraficaRoutingModule
+  ]
+})
+export class AnagraficaModule { }
---
+++ src/app/features/dichiarazione/dichiarazione.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DichiarazioneRoutingModule } from './dichiarazione-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DichiarazioneRoutingModule
+  ]
+})
+export class DichiarazioneModule { }
